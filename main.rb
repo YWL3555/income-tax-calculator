8901c992d6fe8b37464c80436ddc33732416a8c7
Yong Wen Li <yongwenli@Yongs-MacBook-Pro.local>
2023-02-02 07:59:47 +0800

    Update employee class and update unit tests

--- main.rb
+++ main.rb
@@ -30,7 +30,7 @@ class TaxCalculator
   end
   
   class Employee
-    attr_reader :name, :annual_salary
+    attr_reader :name, :annual_salary, :gross_monthly_income, :monthly_tax, :net_monthly_income
   
     def initialize(name, annual_salary)
       @name = name
@@ -42,10 +42,10 @@ class TaxCalculator
       @monthly_tax = (TaxCalculator.new(annual_salary).tax / 12.0).round(2)
       @net_monthly_income = (@gross_monthly_income - @monthly_tax).round(2)
 
-      print_employee_details
-      
+      print_employee_details      
     end
 
+    private
     def print_employee_details
       puts "Monthly Payslip for: #{@name}"
       puts "Gross Monthly Income: $#{sprintf('%.2f',@gross_monthly_income)}"
--- unit-tests.rb
+++ unit-tests.rb
@@ -18,22 +18,28 @@ end
 
 
 class EmployeeTest < Minitest::Test
+
   def test_generate_monthly_payslip_60000
-    employee = Employee.new("Ren", 60000)
-    assert_output(
-        "Monthly Payslip for: Ren\nGross Monthly Income: $5000.00\nMonthly Income Tax: $500.00\nNet Monthly Income: $4500.00\n"
-        ) { employee.generate_monthly_payslip }
+    employee = Employee.new("Jack", 60000)
+    employee.generate_monthly_payslip
+    assert_equal 5000.00, employee.gross_monthly_income
+    assert_equal 500.00, employee.monthly_tax
+    assert_equal 4500.00, employee.net_monthly_income
   end
+
   def test_generate_monthly_payslip_200000
-    employee = Employee.new("Jack", 200000)
-    assert_output(
-        "Monthly Payslip for: Jack\nGross Monthly Income: $16666.67\nMonthly Income Tax: $4000.00\nNet Monthly Income: $12666.67\n"
-        ) { employee.generate_monthly_payslip }
+    employee = Employee.new("Ren", 200000)
+    employee.generate_monthly_payslip
+    assert_equal 16666.67, employee.gross_monthly_income
+    assert_equal 4000.00, employee.monthly_tax
+    assert_equal 12666.67, employee.net_monthly_income
   end
+
   def test_generate_monthly_payslip_80150
     employee = Employee.new("Mark", 80150)
-    assert_output(
-        "Monthly Payslip for: Mark\nGross Monthly Income: $6679.17\nMonthly Income Tax: $837.08\nNet Monthly Income: $5842.09\n"
-        ) { employee.generate_monthly_payslip }
+    employee.generate_monthly_payslip
+    assert_equal 6679.17, employee.gross_monthly_income
+    assert_equal 837.08, employee.monthly_tax
+    assert_equal 5842.09, employee.net_monthly_income
   end
 end
