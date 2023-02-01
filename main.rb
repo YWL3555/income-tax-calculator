d08fec9de55bd044bab3d3b2718dca4a51d1f84b
Yong Wen Li <yongwenli@Yongs-MacBook-Pro.local>
2023-02-02 07:25:40 +0800

    Seperate out function of returing output on console

--- main.rb
+++ main.rb
@@ -38,14 +38,19 @@ class TaxCalculator
     end
   
     def generate_monthly_payslip
-      gross_monthly_income = (annual_salary / 12.0).round(2)
-      monthly_tax = (TaxCalculator.new(annual_salary).tax / 12.0).round(2)
-      net_monthly_income = (gross_monthly_income - monthly_tax).round(2)
-  
-      puts "Monthly Payslip for: #{name}"
-      puts "Gross Monthly Income: $#{sprintf('%.2f',gross_monthly_income)}"
-      puts "Monthly Income Tax: $#{sprintf('%.2f',monthly_tax)}"
-      puts "Net Monthly Income: $#{sprintf('%.2f',net_monthly_income)}"
+      @gross_monthly_income = (@annual_salary / 12.0).round(2)
+      @monthly_tax = (TaxCalculator.new(annual_salary).tax / 12.0).round(2)
+      @net_monthly_income = (@gross_monthly_income - @monthly_tax).round(2)
+
+      print_employee_details
+      
+    end
+
+    def print_employee_details
+      puts "Monthly Payslip for: #{@name}"
+      puts "Gross Monthly Income: $#{sprintf('%.2f',@gross_monthly_income)}"
+      puts "Monthly Income Tax: $#{sprintf('%.2f',@monthly_tax)}"
+      puts "Net Monthly Income: $#{sprintf('%.2f',@net_monthly_income)}"
     end
   end
   
