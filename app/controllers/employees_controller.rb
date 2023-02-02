4639805995a8d02fc4b39a58ec2c21cb3ff2af2b
Yong Wen Li <yongwenli@Yongs-MacBook-Pro.local>
2023-02-02 16:43:54 +0800

    Add parameter validation to ensure the annual salary can only be positive number

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,20 +1,27 @@
 class EmployeesController < ApplicationController
     skip_before_action :verify_authenticity_token
     def monthly_payslip
-      employee = Employee.new(params[:employee_name], params[:annual_salary].to_f)
-      employee.generate_monthly_payslip
-      TaxRecord.create!(
-        timestamp: Time.now.to_s, 
-        employee_name: employee.name, 
-        annual_salary: sprintf('%.2f', employee.annual_salary).to_s, 
-        monthly_income_tax: sprintf('%.2f', employee.monthly_tax).to_s
-      )
-      render json: {
-        employee_name: employee.name,
-        gross_monthly_income: employee.gross_monthly_income,
-        monthly_income_tax: employee.monthly_tax,
-        net_monthly_income: employee.net_monthly_income
-      }
+      employee_name = params[:employee_name]
+      annual_salary = params[:annual_salary].to_f
+
+      if annual_salary <= 0
+        render json: { error: "Annual salary must be a positive number." }, status: :bad_request
+      else
+        employee = Employee.new(employee_name, annual_salary)
+        employee.generate_monthly_payslip
+        TaxRecord.create!(
+          timestamp: Time.now.to_s, 
+          employee_name: employee.name, 
+          annual_salary: sprintf('%.2f', employee.annual_salary).to_s, 
+          monthly_income_tax: sprintf('%.2f', employee.monthly_tax).to_s
+        )
+        render json: {
+          employee_name: employee.name,
+          gross_monthly_income: employee.gross_monthly_income,
+          monthly_income_tax: employee.monthly_tax,
+          net_monthly_income: employee.net_monthly_income
+        }
+      end
     end
   end
 
