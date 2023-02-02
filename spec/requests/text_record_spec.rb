63bc202083e7d64411839df54bc6a727fa2b8cb6
Yong Wen Li <yongwenli@Yongs-MacBook-Pro.local>
2023-02-02 20:41:39 +0800

    Update test case to check the GET API request

--- spec/requests/text_record_spec.rb
+++ spec/requests/text_record_spec.rb
@@ -2,11 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "Tax Records", type: :request do
     describe "GET /tax_records" do
-      
+        before do
+            post "/monthly_payslip", params: { employee_name: "First", annual_salary: 10000 }
+            post "/monthly_payslip", params: { employee_name: "Second", annual_salary: 10000 }
+            post "/monthly_payslip", params: { employee_name: "Third", annual_salary: 10000 }
+        end
+        
         it "returns a list of tax records" do
           get "/tax_records"
-      
           expect(response).to have_http_status(200)
-          end
+          json = JSON.parse(response.body)
+          expect(json["tax_records"].length).to eq(3)
+        end
     end
 end
