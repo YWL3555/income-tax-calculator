a392be2f5d53eeba28bd717c13c332c279034ab3
Yong Wen Li <yongwenli@Yongs-MacBook-Pro.local>
2023-02-03 07:21:17 +0800

    Import JQuery and display response using AJAX

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ ruby "3.2.0"
 gem 'rspec-rails'
 
 gem 'tailwindcss-rails'
+gem 'jquery-rails'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.4", ">= 7.0.4.2"
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,30 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+//= require jquery
+
 import "@hotwired/turbo-rails"
 import "controllers"
+
+$(document).ready(function(){
+    $("#payslip").submit(function(e) {
+    
+        e.preventDefault(); // avoid to execute the actual submit of the form.
+    
+        var form = $(this);
+        var actionUrl = form.attr('action');
+        
+        $.ajax({
+            type: "POST",
+            url: actionUrl,
+            data: form.serialize(), // serializes the form's elements.
+            success: function(data)
+            {   
+               $("#employeeName").text(data.employee_name);
+               $("#grossMonthlyIncome").text(data.gross_monthly_income);
+               $("#monthlyIncomeTax").text(data.monthly_income_tax);
+               $("#netMonthlyIncome").text(data.net_monthly_income);
+            }
+        });
+        
+    });
+    });
