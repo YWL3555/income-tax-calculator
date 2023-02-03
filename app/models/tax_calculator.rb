c765de739f9ec001a978ac34d24334a5f9eeaa4d
Yong Wen Li <yongwenli@Yongs-MacBook-Pro.local>
2023-02-03 13:12:28 +0800

    Implement caching to optimize the performance of calculating tax

--- app/models/tax_calculator.rb
+++ app/models/tax_calculator.rb
@@ -1,3 +1,9 @@
+require 'redis'
+
+$redis = Redis.new(host: "localhost")
+
+$redis.flushdb
+
 class TaxCalculator
     TAX_BRACKET = [[20000, 0], [40000, 0.1], [80000, 0.2], [180000, 0.3], [Float::INFINITY, 0.4]]
   
@@ -12,19 +18,27 @@ class TaxCalculator
     private
   
     def calc_tax(income)
-      total_tax = 0
-      last_amount = 0
-  
-      TAX_BRACKET.each do |amount, tax_rate|
-        if income < amount
-          total_tax += (income - last_amount) * tax_rate
-          break
-        else
-          total_tax += (amount - last_amount) * tax_rate
-          last_amount = amount
+      
+
+      if $redis.exists(income) > 0
+        total_tax = $redis.get(income).to_f
+        puts "Tax retrived from redis"
+      else
+        total_tax = 0
+        last_amount = 0
+        puts "Start calculating"
+        TAX_BRACKET.each do |amount, tax_rate|
+          if income < amount
+            total_tax += (income - last_amount) * tax_rate
+            break
+          else
+            total_tax += (amount - last_amount) * tax_rate
+            last_amount = amount
+          end
         end
-      end
-  
+        puts "Set tax to redis"
+        $redis.set(income, total_tax)
+      end  
       total_tax
     end
 end
