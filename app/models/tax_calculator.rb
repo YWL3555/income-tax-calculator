82ce76cca3b66d04d292b8661eee14798615d537
Yong Wen Li <yongwenli@Yongs-MacBook-Pro.local>
2023-02-15 16:54:48 +0800

    Update tax bracket to latest malaysia tax rate

--- app/models/tax_calculator.rb
+++ app/models/tax_calculator.rb
@@ -1,5 +1,5 @@
 class TaxCalculator
-    TAX_BRACKET = [[20000, 0], [40000, 0.1], [80000, 0.2], [180000, 0.3], [Float::INFINITY, 0.4]]
+    TAX_BRACKET = [[5000, 0], [20000, 0.01], [35000, 0.03], [50000, 0.08], [70000, 0.13], [100000, 0.21], [250000, 0.24], [400000, 0.245],[600000, 0.25], [1000000, 0.26], [2000000, 0.28],[Float::INFINITY, 0.3]]
   
     def initialize(annual_salary)
       @annual_salary = annual_salary
